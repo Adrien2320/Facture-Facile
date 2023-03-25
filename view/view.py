e12475103ffeb9d7f2c88eba7bdf8e323dbb9cf7
Adrien2320 <mertensadrien23@hotmail.be>
2023-03-25 17:03:50 +0100

    modification du style et création de l'affichage pour enregistrer un article

--- view/view.py
+++ view/view.py
@@ -3,7 +3,7 @@ import ttkbootstrap.constants as cttk
 
 
 class MainView(ttk.Window):
-    def __init__(self, title: str,height : int , width : int):
+    def __init__(self, title: str, height: int, width: int):
         super().__init__(themename="superhero")
         self.title(title)
         self.geometry(f"{width}x{height}")
@@ -18,32 +18,48 @@ class MainView(ttk.Window):
         self.data_frame.pack(side=cttk.RIGHT, fill=cttk.BOTH, expand=True)
         # widget button
         self.bt_item = ttk.Button(
-            main_menu_frame, text="ARTICLE", command=self.do_show_article
+            main_menu_frame, text="Article", command=self.do_show_article, width=10
         )
         self.bt_customer = ttk.Button(
-            main_menu_frame, text="CLIENT", command=self.do_show_customer
+            main_menu_frame, text="Client", command=self.do_show_customer, width=10
         )
         self.bt_invoice = ttk.Button(
-            main_menu_frame, text="FACTURE", command=self.do_show_invoice
+            main_menu_frame, text="Facture", command=self.do_show_invoice, width=10
         )
         self.bt_close = ttk.Button(
-            main_menu_frame, text="QUITTER", command=self.do_close
+            main_menu_frame, text="Quitter", command=self.do_close, width=10
         )
+        self.bt_apropos = ttk.Button(main_menu_frame, text="Apropos", width=10)
+        self.bt_setting = ttk.Button(main_menu_frame, text="Paramettre", width=10)
         # position widget
-        self.bt_item.pack(side=cttk.TOP)
-        self.bt_customer.pack(side=cttk.TOP)
-        self.bt_invoice.pack(side=cttk.TOP)
-        self.bt_close.pack(side=cttk.BOTTOM)
+        self.bt_item.pack(side=cttk.TOP, pady=30, padx=10)
+        self.bt_customer.pack(side=cttk.TOP, padx=10)
+        self.bt_invoice.pack(side=cttk.TOP, pady=30, padx=10)
+        self.bt_close.pack(side=cttk.BOTTOM, pady=20, padx=10)
+        self.bt_apropos.pack(side=cttk.BOTTOM, padx=10)
+        self.bt_setting.pack(side=cttk.BOTTOM, pady=20, padx=10)
 
     def start_main_view(self):
         self.mainloop()
 
     def do_show_article(self):
-        MenuArticle(self.option_menu_frame, self.data_frame,self.bt_item,self.bt_customer,self.bt_invoice,self.bt_close)
-        self.bt_item.configure(state='disabled')
-        self.bt_customer.configure(state='disabled')
-        self.bt_invoice.configure(state='disabled')
-        self.bt_close.configure(state='disabled')
+        MenuArticle(
+            self.option_menu_frame,
+            self.data_frame,
+            self.bt_item,
+            self.bt_customer,
+            self.bt_invoice,
+            self.bt_close,
+            self.bt_apropos,
+            self.bt_setting,
+        )
+        self.bt_item["state"] = "disable"
+        self.bt_customer["state"] = "disable"
+        self.bt_invoice["state"] = "disable"
+        self.bt_close["state"] = "disable"
+        self.bt_apropos["state"] = "disable"
+        self.bt_setting["state"] = "disable"
+
     def do_show_customer(self):
         pass
 
@@ -55,7 +71,17 @@ class MainView(ttk.Window):
 
 
 class MenuArticle:
-    def __init__(self, menu_frame: ttk.Frame, data_frame: ttk.Frame,bt_item : ttk.Button,bt_customer : ttk.Button, bt_invoice : ttk.Button, bt_close : ttk.Button):
+    def __init__(
+        self,
+        menu_frame: ttk.Frame,
+        data_frame: ttk.Frame,
+        bt_item: ttk.Button,
+        bt_customer: ttk.Button,
+        bt_invoice: ttk.Button,
+        bt_close: ttk.Button,
+        bt_apropos: ttk.Button,
+        bt_setting: ttk.Button,
+    ):
         # variable get from main menu frame
         self.menu_frame = menu_frame
         self.data_Frame = data_frame
@@ -63,41 +89,54 @@ class MenuArticle:
         self.bt_customer_main_menu = bt_customer
         self.bt_invoice_main_menu = bt_invoice
         self.bt_close_main_menu = bt_close
+        self.bt_apropos_main_menu = bt_apropos
+        self.bt_setting_main_menu = bt_setting
         # widget button
-        self.bt_add = ttk.Button(menu_frame, text="AJOUTER", command=self.do_show_add_data)
+        self.bt_add = ttk.Button(
+            menu_frame, text="Ajouter", command=self.do_show_add_data, width=15
+        )
         self.bt_change = ttk.Button(
-            menu_frame, text="MODIFIER", command=self.do_show_change_data
+            menu_frame, text="Modifier", command=self.do_show_change_data, width=15
         )
         self.bt_remove = ttk.Button(
-            menu_frame, text="SUPPRIMER", command=self.do_show_remove_data
+            menu_frame, text="Supprimer", command=self.do_show_remove_data, width=15
         )
         self.bt_item_search = ttk.Button(
-            menu_frame, text="RECHERCHER", command=self.do_show_search_item_data
+            menu_frame,
+            text="Rechercher",
+            command=self.do_show_search_item_data,
+            width=15,
         )
         self.bt_back = ttk.Button(
-            menu_frame, text="RETOUR", command=self.do_back_menu
+            menu_frame, text="Retour", command=self.do_back_menu, width=15
         )
         # position button
-        self.bt_add.pack(side=cttk.TOP)
-        self.bt_change.pack(side=cttk.TOP)
-        self.bt_remove.pack(side=cttk.TOP)
-        self.bt_item_search.pack(side=cttk.TOP)
-        self.bt_back.pack(side=cttk.BOTTOM)
+        self.bt_add.pack(side=cttk.TOP, padx=10, pady=20)
+        self.bt_change.pack(side=cttk.TOP, padx=10)
+        self.bt_remove.pack(side=cttk.TOP, padx=10, pady=20)
+        self.bt_item_search.pack(side=cttk.TOP, padx=10)
+        self.bt_back.pack(side=cttk.BOTTOM, padx=10, pady=20)
 
     def do_show_add_data(self):
+        #hide the menu frame
+        self.bt_add["state"] = "disable"
+        self.bt_change["state"] = "disable"
+        self.bt_remove["state"] = "disable"
+        self.bt_item_search["state"] = "disable"
+        self.bt_back["state"] = "disable"
         # split window in multiple frames
         self.top_frame = ttk.Frame(self.data_Frame)
         self.bottom_frame = ttk.Frame(self.data_Frame)
         # position frames
-        self.top_frame.pack(side=cttk.TOP,expand=True,fill=cttk.BOTH)
-        self.bottom_frame.pack(side=cttk.BOTTOM,expand=True,fill=cttk.X)
+        self.top_frame.pack(side=cttk.TOP, expand=True, fill=cttk.BOTH)
+        self.bottom_frame.pack(side=cttk.BOTTOM, expand=True, fill=cttk.X)
         # config position
         self.top_frame.columnconfigure(1, weight=2)
-        self.top_frame.rowconfigure(0,weight=1)
-        self.top_frame.rowconfigure(5,weight=1)
+        self.top_frame.rowconfigure(0, weight=1)
+        self.top_frame.rowconfigure(5, weight=1)
         # widget button
         bt_confirm = ttk.Button(self.bottom_frame, text="CONFIRMATION")
-        bt_back= ttk.Button(self.bottom_frame, text="RETOUR")
+        bt_back = ttk.Button(self.bottom_frame, text="RETOUR", command=self.do_back_menu_option)
         # widget label
         lb_top_empty = ttk.Label(self.top_frame)
         lb_bottom_empty = ttk.Label(self.top_frame)
@@ -111,20 +150,20 @@ class MenuArticle:
         en_prix_htva = ttk.Entry(self.top_frame)
         en_taux_tva = ttk.Entry(self.top_frame)
         # position label
-        lb_top_empty.grid(columnspan=2,row=0,sticky=cttk.NSEW)
-        lb_name.grid(column=0, row=1,pady=10)
-        lb_description.grid(column=0, row=2,pady=10)
-        lb_prix_htva.grid(column=0, row=3,pady=10)
-        lb_taux_tva.grid(column=0, row=4,pady=10)
-        lb_bottom_empty.grid(columnspan=2,row=5,sticky=cttk.NSEW)
+        lb_top_empty.grid(columnspan=2, row=0, sticky=cttk.NSEW)
+        lb_name.grid(column=0, row=1, pady=10)
+        lb_description.grid(column=0, row=2, pady=10)
+        lb_prix_htva.grid(column=0, row=3, pady=10)
+        lb_taux_tva.grid(column=0, row=4, pady=10)
+        lb_bottom_empty.grid(columnspan=2, row=5, sticky=cttk.NSEW)
         # position entry
-        en_name.grid(column=1, row=1, sticky=cttk.EW,pady=10)
-        en_description.grid(column=1, row=2, sticky=cttk.EW,pady=10)
-        en_prix_htva.grid(column=1, row=3, sticky=cttk.EW,pady=10)
-        en_taux_tva.grid(column=1, row=4, sticky=cttk.EW,pady=10)
+        en_name.grid(column=1, row=1, sticky=cttk.EW, pady=10, padx=10)
+        en_description.grid(column=1, row=2, sticky=cttk.EW, pady=10, padx=10)
+        en_prix_htva.grid(column=1, row=3, sticky=cttk.EW, pady=10, padx=10)
+        en_taux_tva.grid(column=1, row=4, sticky=cttk.EW, pady=10, padx=10)
         # position button
-        bt_confirm.pack(side=cttk.RIGHT,fill=cttk.X,expand=True,padx=20)
-        bt_back.pack(side=cttk.LEFT,fill=cttk.X,expand=True,padx=20)
+        bt_confirm.pack(side=cttk.RIGHT, fill=cttk.X, expand=True, padx=20)
+        bt_back.pack(side=cttk.LEFT, fill=cttk.X, expand=True, padx=20)
 
     def do_show_change_data(self):
         pass
@@ -141,7 +180,18 @@ class MenuArticle:
         self.bt_remove.destroy()
         self.bt_item_search.destroy()
         self.bt_back.destroy()
-        self.bt_item_main_menu['state'] ='normal'
-        self.bt_customer_main_menu['state'] = 'normal'
-        self.bt_invoice_main_menu['state'] = 'normal'
-        self.bt_close_main_menu['state'] = 'normal'
+        self.bt_item_main_menu["state"] = "normal"
+        self.bt_customer_main_menu["state"] = "normal"
+        self.bt_invoice_main_menu["state"] = "normal"
+        self.bt_close_main_menu["state"] = "normal"
+        self.bt_apropos_main_menu["state"] = "normal"
+        self.bt_setting_main_menu["state"] = "normal"
+
+    def do_back_menu_option(self):
+        self.top_frame.pack_forget()
+        self.bottom_frame.pack_forget()
+        self.bt_add["state"] = "normal"
+        self.bt_change["state"] = "normal"
+        self.bt_remove["state"] = "normal"
+        self.bt_item_search["state"] = "normal"
+        self.bt_back["state"] = "normal"
